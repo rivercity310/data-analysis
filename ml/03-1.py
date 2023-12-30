bf6c9ca2e8088c87d0beaba61c3d1b58fe1948bd
rivercity310 <h970126@gmail.com>
2023-12-30 18:31:23 +0900

    [ML] LinearRegression

--- ml/03-1.py
+++ ml/03-1.py
@@ -4,37 +4,14 @@
 # - 회귀: 샘플을 어떤 임의의 숫자로 예측
 
 # [K-최근접 이웃 분류]: 샘플에 가장 가까운 K개를 골라 다수에 해당하는 클래스로 해당 샘플을 예측
-# [K-최근접 이웃 회귀]: 샘플에 가장 가까운 K개의 수치를 평균 내어 값 예측
-import numpy as np
+# [K-최근접 이웃 회귀]: 샘플에 가장 가까운 K개의 수치를 평균 내어 값 예측 
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error
+from sample import get_sample_data, perch_length, perch_weight
 
 
-perch_length = np.array([8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0, 21.0,
-       21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5, 22.5, 22.7,
-       23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5, 27.3, 27.5, 27.5,
-       27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0, 36.5, 36.0, 37.0, 37.0,
-       39.0, 39.0, 39.0, 40.0, 40.0, 40.0, 40.0, 42.0, 43.0, 43.0, 43.5,
-       44.0])
-
-perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 110.0,
-       115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 130.0,
-       150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 197.0,
-       218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0, 514.0,
-       556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0, 820.0,
-       850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 1000.0,
-       1000.0])
-
-train_input, test_input, train_target, test_target = train_test_split(
-    perch_length, perch_weight, random_state=42
-)
- 
-# 훈련 세트는 2차원 배열이여야 하므로 shape 변경
-train_input = train_input.reshape(-1, 1)
-test_input = test_input.reshape(-1, 1)
-
+train_input, test_input, train_target, test_target = get_sample_data()
 
 # 주어진 데이터의 형태를 파악하기 위해 산점도 그려보기
 def ex1():
@@ -94,7 +71,7 @@ def ex3():
     knr.n_neighbors = 3
     knr.fit(train_input, train_target)
     print(knr.score(train_input, train_target))
-    print(knr.score(test_input, test_target))
+    print(knr.score(test_input, test_target))    
 
 
 if __name__ == "__main__":
---
+++ ml/03-2.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sample import get_sample_data
+
+train_input, test_input, train_target, test_target = get_sample_data()
+
+# K-Neighbors 모델의 문제점: 훈련 범위를 벗어난 데이터를 제대로 처리하지 못함
+# 해당 문제를 해결하기 위해 선형 회귀 모델 사용
+def ex1():
+    knr = KNeighborsRegressor()
+    knr.fit(train_input, train_target)
+
+    # 길이가 훈련 범위를 넘어선 농어 무게 예측
+    # 두 값이 모두 동일한 값으로 예측되는 문제점
+    print(knr.predict([[50]]))  
+    print(knr.predict([[70]]))
+
+    # kneighbors(): 가장 가까운 이웃까지의 거리와 샘플의 인덱스 구하기
+    distances, indexes = knr.kneighbors([[50]]) # 50cm 농어의 이웃 구하기
+
+    plt.scatter(train_input, train_target)
+    plt.scatter(train_input[indexes], train_target[indexes], marker='D')
+    plt.scatter(50, 1010, marker="^")
+    
+    plt.xlabel("length")
+    plt.ylabel("weight")
+    plt.show()
+
+
+from sklearn.linear_model import LinearRegression
+
+# 선형 회귀: 특성이 하나인 경우 어떤 직선을 학습하는 알고리즘
+# 즉, 직선의 방정식 y=ax+b에서 훈련을 통해 적절한 a와 b를 찾아낸다.
+def ex2():
+    lr = LinearRegression()
+    lr.fit(train_input, train_target)
+    print(lr.predict([[50]]))
+
+    # 모델 파라미터: 머신러닝 알고리즘이 찾은 값 (모델 기반 학습) <-> 사례 기반 학습
+    # coef_: 기울기, intercept_: 절편
+    print(lr.coef_, lr.intercept_)
+
+    # [ 직선의 방정식 그리기 ]
+    # 1. 훈련 세트의 산점도 그리기 
+    plt.scatter(train_input, train_target)
+    # 2. 찾은 모델 파라미터를 이용하여 두 점을 이은 직선의 방정식 그리기
+    plt.plot([15, 50], [15 * lr.coef_ + lr.intercept_, 50 * lr.coef_ + lr.intercept_])
+    # 3. 50cm 농어 데이터 표시하기
+    plt.scatter(50, 1241.8, marker="^")
+    plt.xlabel("length")
+    plt.ylabel("weight")
+    plt.show()
+
+    # [ 결정 계수 값 확인해보기 ] 
+    # - 훈련 세트의 값이 테스트 세트의 값보다 많이 큼 -> 과대적합?
+    # - 하지만 훈련 세트의 값도 절대적으로 높지 않음
+    # - 따라서 전체적으로 과소적합 되었다고 볼 수 있음.
+    print(lr.score(train_input, train_target))
+    print(lr.score(test_input, test_target))
+
+
+import numpy as np
+
+# 선형 회귀의 과소적합 문제 해결
+# - 1. 무게가 0 이하인 농어는 없다
+# - 2. 데이터의 분포는 직선보다는 곡선에 가깝다.
+# -> 따라서 2차 방정식 형태의 다항 회귀를 사용한다. 
+def ex3():
+    # 2차 방정식의 그래프를 그리기 위해 길이를 제곱한 항을 훈련 세트 첫번째에 추가
+    # np.column_stack을 이용하면 쉽게 컬럼 추가 가능 (넘파이 브로드캐스팅)
+    train_poly = np.column_stack((train_input ** 2, train_input))
+    test_poly = np.column_stack((test_input ** 2, test_input))
+
+    lr = LinearRegression()
+    lr.fit(train_poly, train_target)
+
+    print(lr.predict([[50 ** 2, 50]]))    # 50cm 데이터에 대한 예측값 출력
+    print(lr.coef_, lr.intercept_)        # 기울기값과 절편 출력
+
+    # 산점도와 그래프 그리기
+    # 2차 방정식(곡선)은 짧은 직선을 이어서 그려 표현한다
+    point = np.arange(15, 51)
+    
+    plt.scatter(train_input, train_target)
+    plt.plot(point, lr.coef_[0] * point ** 2 + lr.coef_[1] * point + lr.intercept_)
+
+    # 50cm 농어 데이터 표시하기
+    plt.scatter(50, 1574, marker="^")
+    plt.xlabel("length")
+    plt.ylabel("weight")
+    plt.show()
+
+    # 다시 한번 결정 계수 출력하기 (여전히 과소적합 문제가 발생)
+    print(lr.score(train_poly, train_target))
+    print(lr.score(test_poly, test_target))
+
+
+def ex4():
+
+
+if __name__ == "__main__":
+    ex4()
